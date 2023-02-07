67a0bccefbb277335352534a6bec38cdf2d18e37
Sam <iaiznura@gmail.com>
2023-02-07 21:09:54 +0300

    work is done

--- utils/func.py
+++ utils/func.py
@@ -1,23 +1,6 @@
 import requests
-
-
-class Transactions:
-    def __init__(self, dict_info):
-        self.state = dict_info["state"]
-        self.date = dict_info["date"]
-
-    def __repr__(self):
-        pass
-
-    def get_status(self):
-        pass
-
-    def get_date(self):
-        pass
-
-    def get_description(self):
-        pass
-
+from datetime import datetime
+from pprint import pprint
 
 def get_data(url):
 
@@ -32,11 +15,36 @@ def get_data(url):
         return None, "ERROR: requests.exceptions.JSONDecodeError"
 
 
-def get_filtered_data(data, filtred_from_empty=False):
-    print(len(data))
+def get_filtered_data(data, filtered_empty_from=False):
     data = [x for x in data if "state" in x and x["state"] == "EXECUTED"]
-    print(len(data))
-    if filtred_from_empty:
+    if filtered_empty_from:
         data = [x for x in data if "from" in x]
-    print(len(data))
     return data
+
+
+def get_last_values(data, count_last_values):
+    data = sorted(data, key=lambda x: x["date"], reverse=True)
+    data = data[:count_last_values]
+    return data
+
+
+def get_formatted_data(data):
+    formatted_data = []
+    for x in data:
+        date = datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
+        description = x["description"]
+        from_info, from_bill = "", ""
+        if "from" in x:
+            sender = x["from"].split()
+            from_bill = sender.pop(-1)
+            from_bill = f'{from_bill[:4]} {from_bill[4:6]}** **** {from_bill[-4:]}'
+            from_info = " ".join(sender)
+        to = f"{x['to'].split()[0]} **{x['to'][-4:]}"
+        operation_amount = f"{x['operationAmount']['amount']} {x['operationAmount']['currency']['name']}"
+
+        formatted_data.append(f'{date} {description} \n'
+                              f'{from_info} {from_bill} --> {to}\n'
+                              f'{operation_amount}')
+
+    return formatted_data
+
